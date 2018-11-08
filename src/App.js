def75b6ea5ff4858afa1398eeb920d63f6ac22d5
曹海亮 <1050588656@qq.com>
2018-11-08 18:58:12 +0800

    引入样式

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Button, TextField} from '@material-ui/core/';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            <TextField
+                id="outlined-name"
+                label="快递编号"
+                margin="normal"
+                variant="outlined"
+            />
+            <br/>
+            <Button variant="contained" color="primary">1</Button>
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
